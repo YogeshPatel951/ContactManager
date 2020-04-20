388693834fa3c1dd5fb17dbb91b02c81ad68b162
Yogesh Patel <patelyogesh951@gmail.com>
2020-04-20 16:47:11 +0530

    Updates

---
+++ precache-manifest.d75f2ea176abfdd87cb3a5dbe6a51a8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abbe9ceb20e807b60795f81ea3395433",
+    "url": "/ContactManager/index.html"
+  },
+  {
+    "revision": "d7a487eab6e15e0370cc",
+    "url": "/ContactManager/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "4ffd737bea1e878d44ed",
+    "url": "/ContactManager/static/css/main.a608e5c0.chunk.css"
+  },
+  {
+    "revision": "d7a487eab6e15e0370cc",
+    "url": "/ContactManager/static/js/2.b40ff38f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ContactManager/static/js/2.b40ff38f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ffd737bea1e878d44ed",
+    "url": "/ContactManager/static/js/main.e13c7377.chunk.js"
+  },
+  {
+    "revision": "753d30902947cf8ba1dd",
+    "url": "/ContactManager/static/js/runtime-main.7212b41a.js"
+  }
+]);
